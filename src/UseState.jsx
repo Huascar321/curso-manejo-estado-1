805bcba37664bad51b2063bf3d2ad2f4ea3e875e
Huascar <oxhuascar321@gmail.com>
2022-02-20 16:18:57 -0400

    Introduccion, codigos de seguridad

---
+++ src/UseState.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function UseState({ name }) {
+  const [error, setError] = React.useState();
+
+  return (
+    <div>
+      <h2>Eliminar {name}</h2>
+      <p>Por favor, escribe el código de seguridad para comprobar que quieres eliminar.</p>
+      <input placeholder="Código de seguridad"/>
+      <button>Comprobar</button>
+    </div>
+  );
+}
+
+export { UseState };
